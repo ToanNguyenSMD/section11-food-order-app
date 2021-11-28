bbe1b1f085b77bd84c7ba0a2adac06f2d2e47c11
Toan Nguyen <toan.nguyen@smartdev.com>
2021-11-28 15:07:25 +0700

    header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
+import classes from './Header.module.css'
+import mealsImg from '../../assets/meals.jpeg'
 
 const Header = (props) => {
   return (
    <React.Fragment>
-     <header>
+     <header className={classes.header}>
         <h1>ReactMeals</h1>
         <button>Cart</button>
      </header>
-     <div>
-      <img src="" alt="" />
+     <div className={classes['main-image']}>
+      <img src={mealsImg} alt="" />
      </div>
    </React.Fragment>
   )
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,7 @@
+const HeaderCartButton = (props) => {
+    return(
+        <div></div>
+    )
+}
+
+export default HeaderCartButton
